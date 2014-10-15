0cde206ebb1f62cc150aa52ca488f753d9433380
Thomas <ostrowski.thomas@gmail.com>
2014-10-15 10:12:26 +0200

    Create FairScheduler.java

---
+++ FairScheduler.java
@@ -0,0 +1,29 @@
+package scheduler;
+
+import java.util.List;
+
+import action.Action;
+
+public class FairScheduler extends Scheduler {
+
+	protected Action currentAction;
+	
+	public FairScheduler(List<Action> l, Action currentAction){
+		super(l);
+		this.currentAction = currentAction;
+	}
+
+	
+	
+
+	// TO-DO
+	@Override
+	public void reallyDoStep() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
