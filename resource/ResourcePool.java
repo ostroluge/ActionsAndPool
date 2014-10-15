c41b0291948538694db86accb2006d8a0f04b94e
Thomas <ostrowski.thomas@gmail.com>
2014-10-15 11:38:00 +0200

    Create ResourcePool.java

---
+++ resource/ResourcePool.java
@@ -0,0 +1,46 @@
+package resource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+
+public abstract class ResourcePool<R extends Resource> {
+
+	protected List<R> resourcesInUse;
+	protected List<R> availableResources;
+
+	
+	public ResourcePool(int nbRes){
+		availableResources = new ArrayList<R>();
+		resourcesInUse = new ArrayList<R>();
+		
+		for(int i=0; i<nbRes; i++){
+			availableResources.add(createResource());
+		}
+	}
+	
+	public R provideResource() throws NoSuchElementException {
+		R res;
+		if (availableResources.size() > 0) {
+			res = availableResources.get(0);
+			resourcesInUse.add(res);
+			availableResources.remove(res);
+			return res;
+		} else {
+			throw new NoSuchElementException("No available resources");
+		}
+	}
+
+	public boolean freeResource(R res) throws IllegalArgumentException {
+		availableResources.add(res);
+		return resourcesInUse.remove(res);
+	}
+
+	public abstract R createResource();
+	
+	
+	public String description() {
+		return null;
+	}
+}
