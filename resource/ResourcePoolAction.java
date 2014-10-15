487a8d59b9f93f85eef35c55cdfd51dbe213555a
Thomas <ostrowski.thomas@gmail.com>
2014-10-15 11:38:45 +0200

    Create ResourcePoolAction.java

---
+++ resource/ResourcePoolAction.java
@@ -0,0 +1,25 @@
+package resource;
+
+import action.Action;
+
+public class ResourcePoolAction <R extends Resource> extends Action {
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void reallyDoStep() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
