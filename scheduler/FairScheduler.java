5b76f4309ca758149b38ca32a72b79e1920644d8
AlexLef <alex.lefebvre59@gmail.com>
2014-10-15 11:36:37 +0200

    Update FairScheduler.java

--- scheduler/FairScheduler.java
+++ scheduler/FairScheduler.java
@@ -9,22 +9,18 @@ public class FairScheduler extends Scheduler {
 
 	protected Action currentAction;
 	
+	public FairScheduler(){
+		
+	}
+	
 	public FairScheduler(List<Action> l, Action currentAction){
 		super(l);
 		this.currentAction = currentAction;
 	}
-
 	
-	
-
-	// TO-DO
 	@Override
 	public void reallyDoStep() {
-		// TODO Auto-generated method stub
+		currentAction.reallyDoStep();
 		
 	}
-	
-	
-	
-	
 }
