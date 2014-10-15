159fc006a740d277b471d246f2227738a40ad996
AlexLef <alex.lefebvre59@gmail.com>
2014-10-15 10:20:44 +0200

    Create SequentialScheduler.java

---
+++ scheduler/SequentialScheduler.java
@@ -0,0 +1,24 @@
+package scheduler;
+
+import java.util.List;
+
+import action.Action;
+
+public class SequentialScheduler extends Scheduler {
+
+	public SequentialScheduler(List<Action> l) {
+		super(l);
+	}
+
+	
+
+	// TO-DO
+	@Override
+	public void reallyDoStep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
