5db1565deed689f9d3ea66c67ab0dab77a60010f
AlexLef <alex.lefebvre59@gmail.com>
2014-10-15 10:17:35 +0200

    Create ActionFinishedException.java

---
+++ action/ActionFinishedException.java
@@ -0,0 +1,14 @@
+package action;
+
+@SuppressWarnings("serial")
+public class ActionFinishedException extends Exception {
+
+	public ActionFinishedException(String msg){
+		super(msg);
+	}
+	
+	public ActionFinishedException(){
+		
+	}
+	
+}
