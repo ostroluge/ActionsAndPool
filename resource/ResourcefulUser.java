bf16b7a037ff567a37d57dc26a584d24baa1ea92
AlexLef <alex.lefebvre59@gmail.com>
2014-10-22 08:42:06 +0200

    Create ResourcefulUser.java

---
+++ resource/ResourcefulUser.java
@@ -0,0 +1,15 @@
+package resource;
+
+public class ResourcefulUser<R extends Resource> {
+			protected R resource;
+			
+			public R getResource(){
+				return resource;
+			}
+			public void setResource(R resource){
+				this.resource=resource;
+			}
+			public void resetResource(){
+				this.resource=null;
+			}
+}
