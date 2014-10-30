8f03061252f9ebd4da6a3cba397e828476cc6810
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 18:28:09 +0100

    Update Resource.java

--- resource/Resource.java
+++ resource/Resource.java
@@ -1,7 +1,16 @@
 package resource;
 
+/**
+ * Interface for all the resources that will be used at the pool
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
+
 public interface Resource {
 
-	public String description();
-
+	/**
+	 * Gives a small description of the resource
+	 * @return a description of the resource
+	 */
+	public String getDescription();
 }
