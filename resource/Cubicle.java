24ea23da4076284ee400bd56365bd0ecff935104
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 18:32:30 +0100

    Update Cubicle.java

--- resource/Cubicle.java
+++ resource/Cubicle.java
@@ -1,12 +1,23 @@
 package resource;
 
+/**
+ * This class defines a cubicle where the swimmers will change their clothes
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
+
 public class Cubicle implements Resource {
 
-	public Cubicle(){
-		
+	public Cubicle() {
+
 	}
-	
-	public String description() {
+
+	/**
+	 * It gives a description of the cubible
+	 * 
+	 * @return the description of a cubicle
+	 */
+	public String getDescription() {
 		return "A place to change clothes";
 	}
 }
