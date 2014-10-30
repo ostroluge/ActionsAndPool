c2515289c92037d9428269b961062f0efb621601
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 17:39:09 +0100

    Update ForeseeableAction.java

--- action/ForeseeableAction.java
+++ action/ForeseeableAction.java
@@ -1,27 +1,66 @@
 package action;
 
+/**
+ * This class defines a foreseeable action, which means that we know how many
+ * steps this action is made of.
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
 
 public class ForeseeableAction extends Action {
 
-	protected int  timeRemaining;
-	
-	public ForeseeableAction(int time, int timeRemaining){
+	protected int timeRemaining;
+
+	/**
+	 * Constructor of a ForeseeableAction that initializes a foreseeable action
+	 * with a time and a time remaining given in parameters
+	 * 
+	 * @param time
+	 *            Number of steps to finish the action
+	 * @param timeRemaining
+	 *            Number of steps to finish the foreseeable action
+	 */
+	public ForeseeableAction(int time, int timeRemaining) {
 		super(time);
-		if(time <= 0){
+		if (time <= 0) {
 			throw new IllegalArgumentException();
 		}
 		this.timeRemaining = timeRemaining;
 	}
-	
-	public boolean isReady(){
+
+	public ForeseeableAction() {
+
+	}
+
+	/**
+	 * Indicates if an action is ready
+	 * 
+	 * @return true if and only if the action is ready
+	 */
+	public boolean isReady() {
 		return timeRemaining == time;
 	}
 
-	public boolean isFinished(){
+	/**
+	 * Indicates if an action is finished
+	 * 
+	 * @return true if and only if an action is finished
+	 */
+	public boolean isFinished() {
 		return timeRemaining <= 0;
 	}
 
-	public void reallyDoStep(){
-		timeRemaining --;
+	/**
+	 * It really realizes a step of a foreseeable action
+	 */
+	public void reallyDoStep() {
+		timeRemaining--;
+	}
+
+	/**
+	 * Creates a new ForeseeableAction
+	 */
+	public ForeseeableAction createAction() {
+		return new ForeseeableAction();
 	}
 }
