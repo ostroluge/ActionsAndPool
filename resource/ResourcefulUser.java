cf45a2a853b843fb7bd8d54280dc7c923786d6a3
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 19:09:29 +0100

    Update ResourcefulUser.java

--- resource/ResourcefulUser.java
+++ resource/ResourcefulUser.java
@@ -1,15 +1,18 @@
 package resource;
 
-public class ResourcefulUser<R extends Resource> {
-			protected R resource;
-			
-			public R getResource(){
-				return resource;
-			}
-			public void setResource(R resource){
-				this.resource=resource;
-			}
-			public void resetResource(){
-				this.resource=null;
-			}
+public class ResourcefulUser <R extends Resource> {
+	
+	protected R resource;
+	
+	public R getResource(){
+		return resource;
+	}
+
+	public void setResource(R resource){
+		this.resource = resource;
+	}
+	
+	public void resetResource(){
+		this.resource = null;
+	}
 }
