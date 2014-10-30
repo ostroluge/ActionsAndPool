aa86dd9f647bbf6febfe1c96bb5ba0891d37de84
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 17:43:02 +0100

    Update OneStepAction.java

--- action/OneStepAction.java
+++ action/OneStepAction.java
@@ -1,24 +1,50 @@
 package action;
 
+/**
+ * This class defines a one step action which means that this action only needs
+ * one step to be finished
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
 
 public class OneStepAction extends Action {
 
 	protected boolean end = false;
 
 	public OneStepAction() {
-		
+
 	}
 
+	/**
+	 * Indicates if an action is ready
+	 * 
+	 * @return true if and only if the action is ready
+	 */
 	public boolean isReady() {
 		return !(this.end);
 	}
 
+	/**
+	 * Indicates if an action is finished
+	 * 
+	 * @return true if and only if an action is finished
+	 */
 	public boolean isFinished() {
 		return this.end;
 	}
 
+	/**
+	 * It really realizes the only step of the OneStepAction so the action is
+	 * finished
+	 */
 	public void reallyDoStep() {
 		this.end = true;
 	}
 
+	/**
+	 * Creates a new OneStepAction
+	 */
+	public OneStepAction createAction() {
+		return new OneStepAction();
+	}
 }
