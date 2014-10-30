341f3b292e2330615b2fe9ec6d183c3dee78bc46
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 19:02:14 +0100

    Update FairScheduler.java

--- scheduler/FairScheduler.java
+++ scheduler/FairScheduler.java
@@ -1,24 +1,47 @@
 package scheduler;
 
-
 import java.util.List;
 
 import action.Action;
 
+/**
+ * This class defines a fair scheduler that has a current action once this
+ * action is finished the fair scheduler will take care of the first
+ * non-finished action that is next to this current action
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
+
 public class FairScheduler extends Scheduler {
 
 	protected Action currentAction;
-	
-	public FairScheduler(){
-		
+
+	public FairScheduler() {
+
 	}
-	
-	public FairScheduler(List<Action> l, Action currentAction){
+
+	/**
+	 * Constructor of a fair scheduler which initializes a fair scheduler with a
+	 * list of actions and a current action given in parameters
+	 * 
+	 * @param l
+	 *            List of actions of the scheduler
+	 * @param currentAction
+	 *            Current action of the fair scheduler
+	 */
+	public FairScheduler(List<Action> l, Action currentAction) {
 		super(l);
 		this.currentAction = currentAction;
 	}
-		
+
+	/**
+	 * Realizes a step of the current action's fair scheduler
+	 */
 	public void reallyDoStep() {
 		currentAction.reallyDoStep();
+	}
+
+	public Action createAction() {
+		return null;
 	}
 }
