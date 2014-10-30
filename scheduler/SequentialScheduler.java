b845823eca673aed5de26b5ff2e1b034b185833b
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 19:05:53 +0100

    Update SequentialScheduler.java

--- scheduler/SequentialScheduler.java
+++ scheduler/SequentialScheduler.java
@@ -4,13 +4,34 @@ import java.util.List;
 
 import action.Action;
 
+/**
+ * This class defines a sequential scheduler which takes care of the actions
+ * sequentially
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
+
 public class SequentialScheduler extends Scheduler {
 
+	/**
+	 * Constructor of a sequential scheduler which initializes a sequential
+	 * scheduler with the list of actions given in parameters
+	 * 
+	 * @param l
+	 *            List of actions
+	 */
 	public SequentialScheduler(List<Action> l) {
 		super(l);
 	}
 
+	/**
+	 * Realizes the step of the first action of the list
+	 */
 	public void reallyDoStep() {
 		listAct.get(0).reallyDoStep();
+	}
+
+	public Action createAction() {
+		return null;
 	}
 }
