14a30a252d452bdde57fa0b095a69cce8aa0bac4
Thomas <ostrowski.thomas@gmail.com>
2014-10-30 17:26:39 +0100

    Update ActionFinishedException.java
    Ajout de la javadoc

--- action/ActionFinishedException.java
+++ action/ActionFinishedException.java
@@ -1,13 +1,26 @@
 package action;
 
+/**
+ * Exception class that is used when an action is finished
+ * 
+ * @author Alexandre LEFEBVRE - Thomas OSTROWSKI
+ */
+
 @SuppressWarnings("serial")
 public class ActionFinishedException extends Exception {
 
-	public ActionFinishedException(String msg){
+	/**
+	 * Constructor of the ActionFinishedException, it initializes the exception
+	 * with a message given in parameters
+	 * 
+	 * @param msg
+	 *            Message that will be displayed when the exception will trigger
+	 */
+	public ActionFinishedException(String msg) {
 		super(msg);
 	}
-	
-	public ActionFinishedException(){
-		
+
+	public ActionFinishedException() {
+
 	}
 }
