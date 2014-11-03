ba57aa5039c8863126ee295ee99b944e3707dd19
Thomas <ostrowski.thomas@gmail.com>
2014-11-03 16:51:31 +0100

    Update TakeResourceAction.java

--- resource/TakeResourceAction.java
+++ resource/TakeResourceAction.java
@@ -1,32 +1,39 @@
 package resource;
+import action.Action;
 import action.OneStepAction;
 
-public class TakeResourceAction extends ResourcePoolAction <Resource>{
+public class TakeResourceAction <R extends Resource> extends Action {
 	
-	protected Resource r;
-	
-	public TakeResourceAction(){
-		
+	protected ResourcePool<R> pool;
+	protected ResourcefulUser<R> user;
+	protected int tmp1, tmp2;
+
+	public TakeResourceAction(ResourcePool<R> p, ResourcefulUser<R> ru){
+		this.pool = p;
+		this.user = ru;
 	}
 	
-	public TakeResourceAction(Resource r){
-		this.r = r;
-	}
-	
-	public void reallyDoStep(){
+	public void reallyDoStep() {
+		tmp1 = this.pool.resourcesInUse.size();
 		if(tryAction()){
-			super.user.setResource(super.pool.provideResource());
+			this.user.setResource(this.pool.provideResource());
+			tmp2 = this.pool.resourcesInUse.size();
 		}
 	}
 	
 	public boolean tryAction(){
-		return (!super.pool.availableResources.isEmpty());
+		return (!this.pool.availableResources.isEmpty());
 	}
-/*	
-	public Resource provideResource(){
-	}
-*/
+	
 	public OneStepAction createAction() {
 		return new OneStepAction();
+	}
+
+	public boolean isReady() {
+		return tryAction();
+	}
+
+	public boolean isFinished() {
+		return tmp2 > tmp1;
 	}
 }
