08e5fb79ab79037c6c7321da7beb292bcef89e6a
AlexLef <alex.lefebvre59@gmail.com>
2014-11-03 17:10:27 +0100

    Update FreeResourceAction.java

--- resource/FreeResourceAction.java
+++ resource/FreeResourceAction.java
@@ -3,38 +3,43 @@ package resource;
 import action.Action;
 import action.OneStepAction;
 
-public class FreeResourceAction <R extends Resource> extends Action {
+public class FreeResourceAction<R extends Resource> extends Action {
 		
-	protected ResourcePool<R> pool;
-	protected ResourcefulUser<R> user;
-	protected int tmp1, tmp2;
 	
-	public FreeResourceAction(ResourcePool<R> p, ResourcefulUser<R> ru){
-		this.pool = p;
-		this.user = ru;
+	protected int tmp1,tmp2;
+	protected ResourcePool<R> rp;
+	protected ResourcefulUser<R> ru;
+	public FreeResourceAction(ResourcePool<R> rp,ResourcefulUser<R>ru){
+		this.rp=rp;
+		this.ru=ru;
 	}
-	
-	public void reallyDoStep(){
-		this.user.resetResource();
+	/*public FreeResourceAction(Resource r){
+		this.r = r;
+	}*/
+
+	public void reallyDoStep() {
+		tmp1=this.rp.resourcesInUse.size();
+		rp.freeResource(this.ru.getResource());
+		tmp2=this.rp.resourcesInUse.size();
 	}
 	
 	public boolean tryAction(){
-		return true;
+		return !(this.rp.availableResources.isEmpty());
 	}
 	
-	public void freeResource(){
+	/*public void freeResource(){
 		
-	}
+	}*/
 
 	public OneStepAction createAction() {
 		return new OneStepAction();
 	}
-
+	@Override
 	public boolean isReady() {
-		return tryAction();
+		return true;
 	}
-
+	@Override
 	public boolean isFinished() {
-		return false;
+		return tmp1>tmp2;
 	}
 }
